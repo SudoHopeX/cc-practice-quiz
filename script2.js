b184fadda61c80e70af4aac604ad78700e1b92ac
Krishna Dwivedi <150330334+sudo-hope0529@users.noreply.github.com>
2025-03-22 18:00:24 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,31 @@
+async function sha512(text) {
+   const encoder = new TextEncoder();
+   const data = encoder.encode(text);
+   const hashBuffer = await crypto.subtle.digest('SHA-512', data);
+   const hashArray = Array.from(new Uint8Array(hashBuffer));
+   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+async function processtext(inputText) {
+   try {
+       const response = await fetch('./data.txt');
+       if (!response.ok) {
+           throw new Error(`HTTP error! status: ${response.status}`);
+       }
+       const fileHash = await response.text();
+       const inputHash = await sha512(inputText);
+
+       if (inputHash === fileHash) {
+           const response = await fetch('./questionsIsc2.json');
+           if (!response.ok) {
+               throw new Error(`HTTP error! status: ${response.status}`);
+           }
+           const questions = await response.json();
+           return questions;
+       } else{
+        return [];
+       }
+   } catch (error) {
+       console.error('Error');
+   }
+}
